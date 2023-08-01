7c565dd29330d346b9887dfcc2e7cae7bc8b1447
Zeusina <kachusov_k@outlook.com>
2023-08-01 09:33:55 +0300

    Added first setup for package

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open('README.md', 'r') as f:
+        return f.read()
+
+
+setup(
+    name='petersburgedu_wrap',
+    version='0.0.1',
+    author='zeusina',
+    author_email='kachusov_k@outlook.com',
+    description='This is module for work with petersburgedu website API',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    url='',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3.10',
+        'Operating System :: OS Independent'
+    ],
+    keywords='api python petersburgedu',
+    project_urls={
+        'Documentation': ''
+    },
+    python_requires='>=3.7'
+)

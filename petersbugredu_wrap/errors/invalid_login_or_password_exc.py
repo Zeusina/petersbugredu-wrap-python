81817edd6ce13c1c84960128762b5b8f319aed88
Zeusina <kachusov_k@outlook.com>
2023-08-09 10:49:37 +0300

    Added docstrings

--- petersbugredu_wrap/errors/invalid_login_or_password_exc.py
+++ petersbugredu_wrap/errors/invalid_login_or_password_exc.py
@@ -1,5 +1,9 @@
 class InvalidLoginOrPasswordException(Exception):
     def __init__(self, *args):
+        """
+        Error raised when login or password are incorrect when user log in.
+        :param args:
+        """
         if args:
             self.message = args[0]
         else:

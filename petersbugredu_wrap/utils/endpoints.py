2a68da3cb3505c80d74aa7ff7e5868c87fc24ba7
Zeusina <kachusov_k@outlook.com>
2023-08-09 10:47:32 +0300

    Added endpoint for getting children list

--- petersbugredu_wrap/utils/endpoints.py
+++ petersbugredu_wrap/utils/endpoints.py
@@ -1,1 +1,6 @@
-LOGIN_URL="https://dnevnik2.petersburgedu.ru/api/user/auth/login"
+"""
+File with URL endpoints for petersburg education portal
+"""
+
+LOGIN_URL = "https://dnevnik2.petersburgedu.ru/api/user/auth/login"
+RELATED_CHILD_LIST_URL = "https://dnevnik2.petersburgedu.ru/api/journal/person/related-child-list"

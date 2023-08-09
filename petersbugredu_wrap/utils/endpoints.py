04fb53f503ee18f502d91328039afabc4806ce04
Zeusina <kachusov_k@outlook.com>
2023-08-09 13:22:59 +0300

    Added teacher list url to endpoints

--- petersbugredu_wrap/utils/endpoints.py
+++ petersbugredu_wrap/utils/endpoints.py
@@ -4,3 +4,4 @@ File with URL endpoints for petersburg education portal
 
 LOGIN_URL = "https://dnevnik2.petersburgedu.ru/api/user/auth/login"
 RELATED_CHILD_LIST_URL = "https://dnevnik2.petersburgedu.ru/api/journal/person/related-child-list"
+TEACHER_LIST_URL="https://dnevnik2.petersburgedu.ru/api/journal/teacher/list?p_page={{page}}&p_educations%5B%5D={{education_id}}"

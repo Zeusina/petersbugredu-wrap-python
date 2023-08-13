f91f06a127134317cc574e001b04c4a404ea8b05
zeusina <kachusov_k@outlook.com>
2023-08-13 18:42:55 +0300

    Add Identity class

---
+++ petersbugredu_wrap/types/identity.py
@@ -0,0 +1,9 @@
+class Identity:
+    def __init__(self, id: int, uid: str = None):
+        """
+        Class for implement identity parameter in API
+        :param id: identity id
+        :param uid: identity uid
+        """
+        self.id = id
+        self.uid = uid

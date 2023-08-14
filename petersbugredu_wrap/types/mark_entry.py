7415ef9c9433de393a8db7e6e2d7593ade030c15
zeusina <kachusov_k@outlook.com>
2023-08-14 19:01:30 +0300

    Added MarkEntry class(usage not implemented yet)

---
+++ petersbugredu_wrap/types/mark_entry.py
@@ -0,0 +1,25 @@
+class MarkEntry:
+    def __init__(self, id: int, education_id: int, lesson_id: int, subject_id: int, subject_name: str, date: str,
+                 estimate_value_code: str, estimate_value_name: str, estimate_comment: str):
+        """
+        Class represents mark entry in API
+        :param id:
+        :param education_id:
+        :param lesson_id:
+        :param subject_id:
+        :param subject_name:
+        :param date:
+        :param estimate_value_code:
+        :param estimate_value_name:
+        :param estimate_comment:
+        """
+        self.estimate_comment = estimate_comment
+        self.estimate_value_name = estimate_value_name
+        self.estimate_value_code = estimate_value_code
+        self.date = date
+        self.subject_name = subject_name
+        self.subject_id = subject_id
+        self.lesson_id = lesson_id
+        self.id = id
+        self.education_id = education_id
+        raise NotImplementedError

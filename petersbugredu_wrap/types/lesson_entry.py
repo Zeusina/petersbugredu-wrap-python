a39e0227b7450bf9f00672d95f134e34ef580858
zeusina <kachusov_k@outlook.com>
2023-08-14 19:15:39 +0300

    Added LessonEntry class(usage not implemented yet)

---
+++ petersbugredu_wrap/types/lesson_entry.py
@@ -0,0 +1,37 @@
+from petersbugredu_wrap.types.identity import Identity
+from petersbugredu_wrap.types.task import Task
+
+
+class LessonEntry:
+    def __init__(self, identity: Identity, number: int, datetime_from: str, datetime_to: str, subject_id: int,
+                 subject_name: str, content_name: str, content_description, content_additional_material,
+                 tasks: list[Task], estimates: list, action_payload):
+        """
+        Class represents lesson entry in API
+        :param identity: 
+        :param number: 
+        :param datetime_from: 
+        :param datetime_to: 
+        :param subject_id: 
+        :param subject_name: 
+        :param content_name: 
+        :param content_description: 
+        :param content_additional_material: 
+        :param tasks: 
+        :param estimates: 
+        :param action_payload: 
+        """
+
+        self.action_payload = action_payload
+        self.estimates = estimates
+        self.tasks = tasks
+        self.content_additional_material = content_additional_material
+        self.content_description = content_description
+        self.content_name = content_name
+        self.subject_name = subject_name
+        self.subject_id = subject_id
+        self.datetime_to = datetime_to
+        self.datetime_from = datetime_from
+        self.number = number
+        self.identity = identity
+        raise NotImplementedError

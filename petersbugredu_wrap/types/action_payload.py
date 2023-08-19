2e1e2129d685994c2a55549ff6ffcc1e223db633
zeusina <kachusov_k@outlook.com>
2023-08-19 11:27:27 +0300

    Updated docstring to ActionPayload class

--- petersbugredu_wrap/types/action_payload.py
+++ petersbugredu_wrap/types/action_payload.py
@@ -1,9 +1,10 @@
 class ActionPayload:
     def __init__(self, can_apply_for_distance: bool = True, can_print=None, can_add_homework: bool = True):
         """
-        Class represents action payload API class
+        Class represents action payload API entity
         :param can_apply_for_distance:
         :param can_print:
+        :param can_add_homework:
         """
         self.can_apply_for_distance = can_apply_for_distance
         self.can_print = can_print

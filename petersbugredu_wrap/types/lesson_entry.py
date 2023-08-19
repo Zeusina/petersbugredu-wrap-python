9631902cc47b4764b203e52fd55fa4e258910b90
zeusina <kachusov_k@outlook.com>
2023-08-19 11:08:22 +0300

    Added new type hints in LessonEntry class

--- petersbugredu_wrap/types/lesson_entry.py
+++ petersbugredu_wrap/types/lesson_entry.py
@@ -1,11 +1,16 @@
+import datetime
+
+from petersbugredu_wrap.types import ActionPayload
+from petersbugredu_wrap.types.estimate import Estimate
 from petersbugredu_wrap.types.identity import Identity
 from petersbugredu_wrap.types.task import Task
 
 
 class LessonEntry:
-    def __init__(self, identity: Identity, number: int, datetime_from: str, datetime_to: str, subject_id: int,
-                 subject_name: str, content_name: str, content_description, content_additional_material,
-                 tasks: list[Task], estimates: list, action_payload):
+    def __init__(self, identity: Identity, number: int, datetime_from: datetime.datetime,
+                 datetime_to: datetime.datetime, subject_id: int,subject_name: str, content_name: str,
+                 content_description, content_additional_material, tasks: list[Task], estimates: list[Estimate],
+                 action_payload: ActionPayload):
         """
         Class represents lesson entry in API
         :param identity: 
@@ -34,4 +39,3 @@ class LessonEntry:
         self.datetime_from = datetime_from
         self.number = number
         self.identity = identity
-        raise NotImplementedError

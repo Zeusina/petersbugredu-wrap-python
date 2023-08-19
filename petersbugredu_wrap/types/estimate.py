bde61f40fb99529154c24b7ce84f3060f1a0d17c
zeusina <kachusov_k@outlook.com>
2023-08-19 11:26:37 +0300

    Added docstring to Estimate class

--- petersbugredu_wrap/types/estimate.py
+++ petersbugredu_wrap/types/estimate.py
@@ -1,6 +1,14 @@
 class Estimate:
     def __init__(self, estimate_type_code: str, estimate_type_name: str,
                  estimate_value_code: str, estimate_value_name: str, estimate_comment):
+        """
+        Class represents estimate API entity
+        :param estimate_type_code:
+        :param estimate_type_name:
+        :param estimate_value_code:
+        :param estimate_value_name:
+        :param estimate_comment:
+        """
         self.estimate_type_code = estimate_type_code
         self.estimate_type_name = estimate_type_name
         self.estimate_value_code = estimate_value_code

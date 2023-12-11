613d5ad1f089067dcadd46388ffe287787d8a7ae
Zeusina <kachusov_k@outlook.com>
2023-12-11 18:12:15 +0300

    Added docstrings for modules

--- petersbugredu_wrap/__init__.py
+++ petersbugredu_wrap/__init__.py
@@ -1,2 +1,5 @@
+"""
+Library for accessing petersburg education website via python
+"""
 from petersbugredu_wrap.client import Client
 import petersbugredu_wrap.types
--- petersbugredu_wrap/types/__init__.py
+++ petersbugredu_wrap/types/__init__.py
@@ -1,3 +1,6 @@
+"""
+Module used for storing types of this library
+"""
 from petersbugredu_wrap.types.child import Child
 from petersbugredu_wrap.types.teacher import Teacher
 from petersbugredu_wrap.types.action_payload import ActionPayload

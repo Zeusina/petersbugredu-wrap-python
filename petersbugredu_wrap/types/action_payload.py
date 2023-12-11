d6405a245c857a951e4fc1a000be3fd7f6a692a1
Zeusina <kachusov_k@outlook.com>
2023-12-11 18:16:12 +0300

    Refactor with linter

--- petersbugredu_wrap/types/action_payload.py
+++ petersbugredu_wrap/types/action_payload.py
@@ -1,11 +1,12 @@
 class ActionPayload:
-    def __init__(self, can_apply_for_distance: bool = True, can_print=None, can_add_homework: bool = True):
-        """
-        Class represents action payload API entity
-        :param can_apply_for_distance:
-        :param can_print:
-        :param can_add_homework:
-        """
+    """
+    Class represents action payload API entity
+    :param can_apply_for_distance:
+    :param can_print:
+    :param can_add_homework:
+    """
+    def __init__(self, can_apply_for_distance: bool = True, can_print=None,
+                 can_add_homework: bool = True):
         self.can_apply_for_distance = can_apply_for_distance
         self.can_print = can_print
         self.can_add_homework = can_add_homework
